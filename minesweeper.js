f48dfa382ebe27e090546c8cd4d341a03dd8a634
Noah Chongsiriwatana <noah@chongsiriwatana.net>
2021-01-07 03:07:12 -0500

    right click functionality

--- minesweeper.js
+++ minesweeper.js
@@ -13,7 +13,6 @@ function runMinesweeper(canvID, width) {
 	ctx.fillStyle = "white";
 	ctx.fill();
 	ctx.closePath();
-	
 	var sqNum = 10;
 	var freq = 5;
 	var sqLen = width / sqNum;
@@ -57,8 +56,38 @@ function newGame(size, mineRatio) {
  * width is the width of the square.
  */
 function drawSquare(ctx, pos, width) {
+	width -= 1;
 	ctx.beginPath();
 	ctx.rect(pos.x, pos.y, width, width);
+	ctx.stroke();
+	ctx.fillStyle = "white";
+	ctx.fill();
+}
+
+/*
+ * Draws a flag.
+ *
+ * Parameters:
+ * ctx is the canvas context to draw the flag on.
+ * pos is the relative position of the top right corner.
+ * width is the width of the square.
+ */
+function drawFlag(ctx, pos, width) {
+	ctx.fillStyle = "red";
+	ctx.beginPath();
+	unit = width / 10;
+	start = {
+		x: pos.x + unit,
+		y: pos.y + unit
+	}
+	ctx.moveTo(start.x, start.y);
+	ctx.lineTo(start.x + 4 * unit, start.y + 2 * unit);
+	ctx.lineTo(start.x, start.y + 4 * unit);
+	ctx.fill();
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	ctx.moveTo(start.x, start.y);
+	ctx.lineTo(start.x, start.y + 8 * unit);
 	ctx.stroke();
 }
 
@@ -77,6 +106,16 @@ function gameListen(canvas, game) {
 			alert(loc.x + ", " + loc.y);
 		}
 	);
+	
+	canvas.addEventListener(
+		'contextmenu',
+		function(e) {
+			e.preventDefault();
+			var loc = getLoc(canvas, game, e);
+			alertMine(canvas, game, loc);
+			return false;
+		}
+	);
 }
 
 /*
@@ -90,8 +129,54 @@ function gameListen(canvas, game) {
 function getLoc(canvas, game, e) {
 	var rect = canvas.getBoundingClientRect();
 	return {
-		x: Math.floor((e.clientX - rect.left) / rect.width * game.length) + 1,
-		y: Math.floor((e.clientY - rect.top) / rect.height * game.length) + 1
+		x: Math.floor((e.clientX - rect.left) / rect.width * game.length),
+		y: Math.floor((e.clientY - rect.top) / rect.height * game.length)
+	};
+}
+
+/*
+ * Handles the right click actions.
+ *
+ * Parameters:
+ * canvas is the canvas where the game is drawn.
+ * game is the representative array.
+ * loc is the square's location of the click.
+ */
+function alertMine(canvas, game, loc) {
+	ctx = canvas.getContext('2d');
+	rect = canvas.getBoundingClientRect();
+	width = rect.width / game.length;
+	pos = locToPos(loc, width);
+	switch (game[loc.x][loc.y]) {
+		case 0:
+			game[loc.x][loc.y] = -1;
+			drawFlag(ctx, pos, width);
+			break;
+		case -1:
+			game[loc.x][loc.y] = 0;
+			drawSquare(ctx, pos, width);
+			break;
+		case 1:
+			game[loc.x][loc.y] = 2;
+			drawFlag(ctx, pos, width);
+			break;
+		case 2:
+			game[loc.x][loc.y] = 1;
+			drawSquare(ctx, pos, width);
+	}
+}
+
+/*
+ * Converts a square location to its top right corner.
+ *
+ * Parameters:
+ * loc is the square location.
+ * width is the width of the square.
+ */
+function locToPos(loc, width) {
+	return {
+		x: (loc.x) * width,
+		y: (loc.y) * width
 	};
 }
 
